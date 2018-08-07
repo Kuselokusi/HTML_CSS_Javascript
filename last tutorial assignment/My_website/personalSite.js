642c6e8c422621ce2df54bb96d7aa2b07226b548
Kuselokusi <kn.ntsaluba@gmail.com>
2018-08-07 17:46:18 +0200

    latest code

---
+++ last tutorial assignment/My_website/personalSite.js
@@ -0,0 +1,47 @@
+document.querySelector("#twitter_label").addEventListener("click", function() {
+
+    window.open("https://twitter.com/KuseloKusi", "_blank");
+
+});
+
+document.querySelector("#twitter").addEventListener("click", function() {
+
+    window.open("https://twitter.com/KuseloKusi", "_blank");
+
+});
+
+document.querySelector("#linkedin_label").addEventListener("click", function() {
+
+    window.open("https://www.linkedin.com/in/kuselontsaluba/", "_blank");
+
+});
+
+document.querySelector("#linkedin").addEventListener("click", function() {
+
+    window.open("https://www.linkedin.com/in/kuselontsaluba/", "_blank");
+
+});
+
+document.querySelector("#gitHub_label").addEventListener("click", function() {
+
+    window.open("https://github.com/Kuselokusi", "_blank");
+
+});
+
+document.querySelector("#gitHub").addEventListener("click", function() {
+
+    window.open("https://github.com/Kuselokusi", "_blank");
+
+});
+
+document.querySelector("#instagram_label").addEventListener("click", function() {
+
+    window.open("https://www.instagram.com/kuselokusi/", "_blank");
+
+});
+
+document.querySelector("#instagram").addEventListener("click", function() {
+
+    window.open("https://www.instagram.com/kuselokusi/", "_blank");
+
+});
